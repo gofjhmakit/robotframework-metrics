dd4ff7340ff93b460f198d04a087bad5456dce46
Hermanni Mäkitalo <hermanni.makitalo@gofore.com>
2022-03-31 11:31:06 +0300

    Added Total runtime column per keyword to KW Times page

--- robotframework_metrics/keyword_times.py
+++ robotframework_metrics/keyword_times.py
@@ -1,12 +1,18 @@
 import pandas as pd
 
+
 class KeywordTimes():
 
     def get_keyword_times(self, kw_list):
         keywords_data_frame = pd.DataFrame.from_records(kw_list)
         if not keywords_data_frame.empty:
-            kw_times = (keywords_data_frame.groupby("Name").agg(times = ("Time", "count"), time_min = ("Time", min),
-            time_max = ("Time", max), time_mean = ("Time", "mean")).reset_index())
+            kw_times = (
+                keywords_data_frame.groupby("Name").agg(
+                    times=("Time", "count"),
+                    time_min=("Time", min),
+                    time_max=("Time", max),
+                    time_mean=("Time", "mean"),
+                    time_total=("Time", "sum")).reset_index())
         else:
             kw_times = keywords_data_frame
         return kw_times
